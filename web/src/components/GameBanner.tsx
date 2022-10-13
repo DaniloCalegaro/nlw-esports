b543896762f555200e12bb40f62d8f10cedad681
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-10-13 17:46:52 -0300

    feat: created modal for create ad

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,12 +1,16 @@
-export function GameBanner() {
+interface GameBannerProps {
+  bannerUrl: string
+  title: string
+  adsCount: number
+}
+
+export function GameBanner({ bannerUrl, title, adsCount }: GameBannerProps) {
   return (
     <a href="" className="relative rounded-lg overflow-hidden">
-      <img src="/game-1.png" alt="" />
+      <img src={bannerUrl} alt="" />
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-        <strong className="font-bold text-white block">
-          League of Legends
-        </strong>
-        <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+        <strong className="font-bold text-white block">{title}</strong>
+        <span className="text-zinc-300 text-sm block mt-1">{adsCount}</span>
       </div>
     </a>
   )
